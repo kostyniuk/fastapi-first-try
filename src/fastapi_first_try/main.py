4dc6d719ca36fa69e6684db7025fa7cb93705ba3
Alex Kostyniuk <kostyniuk7@gmail.com>
2026-09-10 19:42:59 +0200

    remove unused test route from main.py

--- src/fastapi_first_try/main.py
+++ src/fastapi_first_try/main.py
@@ -6,10 +6,6 @@ app = FastAPI()
 def read_root():
     return 'Hello FastAPI'
 
-@app.get('/test')
-def read_test():
-    return 'Some test GET route'
-
 @app.get('/posts/{id}/info/{infoId}')
 def read_post(id: int, infoId: int, queryA: str | None = None):
     return f"Post {id}, info {infoId}, q: {str(queryA)}"
